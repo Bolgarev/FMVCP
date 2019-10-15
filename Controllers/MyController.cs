1e5fabe0209a90265eedfc88f7a9744b0b37216b
bbolgarev <bbolgarev@national-platform.ru>
2019-10-15 19:24:55 +0300

    add controllers

---
+++ Controllers/MyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FMVCP.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            var ip = requestContext.HttpContext.Request.UserHostAddress;
+            var response = requestContext.HttpContext.Response;
+            response.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");
+        }
+    }
+}
